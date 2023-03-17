312bda8af1c746b17458e2a14e48af4c706f7348
shahinasak98 <shahinasak98@gmail.com>
2023-03-17 20:55:18 +0530

    Edits

--- bot_project/bot_project/settings.py
+++ bot_project/bot_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--01*$udip^k%%@wjo&^zr-a5c$ofh=hl_r0&bpvs$)y_45ph=4'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
